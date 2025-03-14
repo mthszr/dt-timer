795197aa7fef3e7ce0619c3a1fc293b0aa1db66d
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-14 19:55:38 -0300

    add Transactions page layout with styled components and transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary/indext";
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary/indext'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 150,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome"> - R$ 50,00</PriceHighlight>
+              </td>
+              <td>Lanche</td>
+              <td>25/10/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
